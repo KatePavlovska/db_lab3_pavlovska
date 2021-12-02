81e336898240c1fe1651900e3c632855084f03af
Kate Pavlovska <55051629+KatePavlovska@users.noreply.github.com>
2021-12-02 12:18:36 +0200

    Add files via upload

---
+++ export_json.py
@@ -0,0 +1,27 @@
+import json
+import psycopg2
+
+username = 'pavlovska_kate'
+password = 'postgres'
+database = 'pavlovska_kate_DB'
+host = 'localhost'
+port = '5432'
+
+conn = psycopg2.connect(user=username, password=password, dbname=database, host=host, port=port)
+
+data = {}
+with conn:
+    cur = conn.cursor()
+
+    for table in ('fuels', 'owners', 'powerplants'):
+        cur.execute('SELECT * FROM ' + table)
+        rows = []
+        fields = [x[0] for x in cur.description]
+
+        for row in cur:
+            rows.append(dict(zip(fields, row)))
+
+        data[table] = rows
+
+with open('all_data.json', 'w') as outf:
+    json.dump(data, outf, default=str)
